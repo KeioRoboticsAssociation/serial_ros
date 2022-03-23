1603acf1357095ca35de9704bd1a777efb3f9112
curious <curious.ks.jp@gmail.com>
2022-03-24 03:31:51 +0900

    ちょっと眠い、明日やるわ

--- src/rogi_link.cpp
+++ src/rogi_link.cpp
@@ -1,5 +1,8 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32MultiArray.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Empty.h"
+#include "std_msgs/Char.h"
 
 #include <string>
 #include <unistd.h>
@@ -41,7 +44,7 @@ char *floattochar;
 int floatdatasize = 0;
 
 
-void sub_callback(const std_msgs::Float32MultiArray &serial_msg)
+void sub_callback(const std_msgs::Char &serial_msg)
 {
     if (subflag)
     {
@@ -49,14 +52,14 @@ void sub_callback(const std_msgs::Float32MultiArray &serial_msg)
         subflag == false;
     }
     delete[] floattochar;
-    floatdatasize = serial_msg.data.size();
-    floattochar = new char[floatdatasize * 4 + 6];
-    floattochar[0] = 'f';
-    *(int *)(&floattochar[1]) = floatdatasize;
+    // floatdatasize = serial_msg.data.size(); //ここら辺場合に依ってしまう、他ノードでどこまで指定するか…
+    floattochar = new char[11]; 
+    floattochar[0] = 0xFF;
+    *(int *)(&floattochar[1]) = serial_msg.data[0];//canID
     //memcpy(&floattochar[1], &datasize, 4);
     for (int i = 0; i < floatdatasize; i++)
     {
-        *(float *)(&floattochar[i * 4 + 5]) = serial_msg.data[i];
+        *(float *)(&floattochar[i * 4 + 2]) = serial_msg.data[i+1];
         //memcpy(&floattochar[i * 4 + 5], &serial_msg.data[i], 4);
     }
     floattochar[floatdatasize * 4 + 5] = endmsg;
@@ -66,11 +69,12 @@ void sub_callback(const std_msgs::Float32MultiArray &serial_msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "rogi_serial_node");
+    ros::init(argc, argv, "rogi_link_node");
     ros::NodeHandle n;
 
     //Publisher
     ros::Publisher serial_pub = n.advertise<std_msgs::Float32MultiArray>("Serial_pub", 1000);
+    ros::Publisher connection_status = n.advertise<std_msgs::Empty>("connection_status", 1);
 
     //Subscriber
     ros::Subscriber serial_sub = n.subscribe("Serial_sub", 100, sub_callback);
@@ -82,6 +86,7 @@ int main(int argc, char **argv)
     arg_n.getParam("baudrate", BAUDRATE);
     arg_n.getParam("looprate", sub_loop_rate);
 
+    //opening serial port
     fd1 = open_serial(port_name.c_str());
 
     while (ros::ok())
@@ -89,10 +94,9 @@ int main(int argc, char **argv)
         fd1 = open_serial(port_name.c_str());
         //ROS_ERROR("Serial Fail: cound not open %s", port_name.c_str());
         //ros::shutdown();
-        printf("Serial Connecting\n");
-        sleep(1);
-        if (fd1 >= 0)
-            break;
+        ROS_WARN_ONCE("Serial Connecting\n");
+
+        if (fd1 >= 0)   break;
     }
 
     ROS_INFO("Serial Success");
@@ -123,30 +127,31 @@ int main(int argc, char **argv)
             }
             else if (buf_pub[recv_data_size - 1] == endmsg)
             {
-                arraysize = *(int *)(&buf_pub[1]);
+                // arraysize = *(int *)(&buf_pub[1]);
                 //memcpy(&arraysize, &(buf_pub[1]), 4);
 
-                if(buf_pub[0]==0xFF)
+                if(buf_pub[0]==0xFF) //checking start flag
                 {
-                    if (recv_data_size == arraysize * 4 + 6)
+                    if (recv_data_size == 11) //data length 11byte
                     {
                         std_msgs::Float32MultiArray pub_float;
-                        pub_float.data.resize(arraysize);
+                        pub_float.data.resize(2);
                         for (int i = 0; i < arraysize; i++)
                         {
-                            pub_float.data[i] = *(float *)(&buf_pub[i * 4 + 5]);
+                            pub_float.data[i] = *(float *)(&buf_pub[i * 4 + 2]);
                             //memcpy(&pub_float.data[i], &buf_pub[i * 4 + 5], 4);
                         }
                         serial_pub.publish(pub_float);
+                        //現在は受信はfloatに限定、他用途ができた場合はfloat以外の処理も導入する必要あり
                     }
                     else
                     {
-                        ROS_INFO("Datasize Error Float");
+                        ROS_WARN("Datasize Error");
                     }
+                    //ただしバッファーに複数回分の受信データがストックされてしまった場合についてもDatasize Errorになってしまっている
                 }
 
-
-                else    ROS_INFO("ERROR");
+                else    ROS_WARN("START FLAG ERROR");
             
                 recv_data_size = 0;
             }
@@ -158,10 +163,13 @@ int main(int argc, char **argv)
             rec = write(fd1, floattochar, floatdatasize * 4 + 6);
             if (rec < 0)
             {
-                ROS_ERROR_ONCE("Serial Fail: cound not write float");
+                ROS_ERROR_ONCE("Serial Fail: cound not write");
             }
             subflag = false;
         }
+
+        std_msgs::Empty status_msg;
+        connection_status.publish(status_msg);
 
         ros::spinOnce();
         loop_rate.sleep();
